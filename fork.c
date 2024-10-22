d7a2c959e1da1b0a6c3d0af347722768e666ea49
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-10-22 07:34:14 -0700

    some corrections

--- fork.c
+++ fork.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+
+int g = 0;
 
 int main(void){
     printf("main()\n");
     pid_t res = fork();
-
+    g++;
     if(res == 0){ // child
         printf("child\n");
+        g++;
         exit(0);
     }
 
     // parent
-    printf("parent\n");
+    printf("parent, %d\n", g);
     
     return 0;
 }
